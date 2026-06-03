8d97c0936654fb9ed63ac83e4f985e7938b1c7c0
JonMS95 <jon_portugalete@hotmail.com>
2026-06-03 21:16:49 +0200

    Extended main file.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include "preprocessor.hpp"
 #include "spectral_analyzer.hpp"
+#include "fingerprint_generator.hpp"
 
 int main(int argc, char** argv)
 {
@@ -29,7 +30,22 @@ int main(int argc, char** argv)
     std::vector<float> prep_signal = prep.preprocessData(input, output);
 
     SpectralAnalyzer spectral_analyzer(frame_duration, downsmp_freq, feat_ratio);
-    spectral_analyzer.analyze(prep_signal);
+    std::vector<std::vector<int>> features = spectral_analyzer.analyze(prep_signal);
+
+    FingerprintGenerator fp_generator(10);
+    std::unordered_map<int, std::vector<uint32_t>> hashes = fp_generator.genFP(features);
+
+    for(auto it = hashes.begin(); it != hashes.end(); it++)
+    {
+        std::cout << it->first << std::endl;
+
+        for(uint32_t& h : it->second)
+            std::cout << h << " ";
+    
+        std::cout << std::endl;
+
+        std::cout << "-------------------" << std::endl;
+    }
 
     return 0;
 }
