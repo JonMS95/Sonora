986f267e70a99a96919b7d460d3574980782edba
JonMS95 <jon_portugalete@hotmail.com>
2026-06-08 20:20:09 +0200

    Completed AudioDBMatcher class with queryHashes and _getSongName methods.

--- src/audio_db_matcher.hpp
+++ src/audio_db_matcher.hpp
@@ -1,12 +1,21 @@
 #ifndef AUDIO_DB_MATCHER
 #define AUDIO_DB_MATCHER
 
+#include <string>
+#include <vector>
+#include <cstddef>
+#include <unordered_map>
 #include "audio_db_base.hpp"
 
 class AudioDBMatcher : public AudioDBBase
 {
+private:
+    std::string _getSongName(const uint32_t song_id) const;
+
 public:
-    AudioDBMatcher(const std::string& db_path);
+    explicit AudioDBMatcher(const std::string& db_path);
+
+    std::string queryHashes(const std::unordered_map<std::size_t, std::vector<uint32_t>>& frame_hashes) const;
 };
 
 #endif
