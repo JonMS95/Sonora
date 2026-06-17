6c6a080f49ae00f24089a89df56d336d0dbc6d96
JonMS95 <jon_portugalete@hotmail.com>
2026-06-17 22:39:17 +0200

    Modified options order. Either index or match ops are allowed now (no more need to recompile each time).

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,87 @@
 #include <vector>
 #include <string>
+#include <cstdlib>
 #include <iostream>
 #include "audio_indexer.hpp"
 #include "audio_matcher.hpp"
 
 int main(int argc, char** argv)
 {
-    if(argc <= 2)
+    /*
+    1: index_match -> i/m
+    2: input -> file path (either absolute or relative)
+    3: db_path -> path to database (either absolute or relative)
+    4: downsmp_freq -> int
+    5: feature_ratio -> int
+    6: feature_ratio -> int
+    7: window_size -> int
+    8: peak_number -> int
+    9: fir_coefs -> int
+    */
+    
+    char index_match = 'i';
+    std::string input = "dummy";
+    std::string db_path = "dummy";
+    uint32_t downsmp_freq = 8000;
+    std::size_t fir_coefs = 21;
+    float frame_duration = 0.1f; char* end;
+    uint32_t feature_ratio = 4;
+    uint8_t window_size = 3;
+    uint8_t peak_number = 3;
+
+    if(argc >= 1)
     {
-        // std::cout << "Missing parameters" << std::endl;
-        return 1;
+        if(argv[1][0] != 'i' && argv[1][0] != 'm')
+        {
+            std::cerr << "Invalid input for index/match (must be either i or m, no other)." << std::endl;
+            return -1;
+        }
+
+        index_match = argv[1][0];
+    }
+        
+    if(argc >= 2)
+        input = std::string(argv[2]);
+
+    if(argc >= 3)
+        db_path = std::string(argv[3]);
+    
+    if(argc >= 4)
+        downsmp_freq = atoi(argv[4]);
+    
+    if(argc >= 5)
+        fir_coefs = atoi(argv[5]);
+
+    if(argc >= 6)
+    {
+        frame_duration = std::strtof(argv[6], &end);
+
+        if (*end != '\0')
+        {
+            std::cerr << "Invalid frame duration: " << argv[6] << '\n';
+            return -1;
+        }
     }
 
-    const std::string& input    = std::string(argv[1]);
-    const std::string& output   = std::string(argv[2]);
+    if(argc >= 7)
+        feature_ratio = atoi(argv[7]);
     
-    uint32_t downsmp_freq = 16000;
+    if(argc >= 8)
+        window_size = atoi(argv[7]);
 
-    std::string db_path = std::string("fingerprints.db");
+    if(argc >= 9)
+        peak_number = atoi(argv[7]);
 
-    if(argc > 3)
-        downsmp_freq = atoi(argv[3]);
+    if(index_match == 'i')
+    {
+        AudioIndexer audio_indexer(downsmp_freq, db_path, fir_coefs, frame_duration, feature_ratio, window_size, peak_number);
+        audio_indexer.index(input);
+    }
+    else
+    {
+        AudioMatcher audio_matcher(downsmp_freq, db_path, fir_coefs, frame_duration, feature_ratio, window_size, peak_number);
+        std::cout << audio_matcher.match(input) << std::endl;
+    }
     
-    if(argc > 4)
-        db_path = std::string(argv[4]);
-
-    const std::size_t   fir_coefs       = 21;
-    const float         frame_duration  = 0.1f;
-    const uint32_t      feature_ratio   = 4;
-    const uint8_t       window_size     = 3;
-    const uint8_t       peak_number     = 3;
-
-    // AudioIndexer audio_indexer(downsmp_freq, db_path, fir_coefs, frame_duration, feature_ratio, window_size, peak_number);
-    // audio_indexer.index(input, output);
-
-    AudioMatcher audio_matcher(downsmp_freq, db_path, fir_coefs, frame_duration, feature_ratio, window_size, peak_number);
-    std::cout << audio_matcher.match(input) << std::endl;
-
     return 0;
 }
