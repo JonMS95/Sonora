df21bb02d045c8f2d688b0ed6a354f02a2e9ee93
JonMS95 <jon_portugalete@hotmail.com>
2026-06-20 17:29:16 +0200

    Called constructor with proper input parameters.

--- src/audio_matcher.cpp
+++ src/audio_matcher.cpp
@@ -9,8 +9,19 @@ AudioMatcher::AudioMatcher( const uint32_t downsmp_freq ,
                             const uint32_t feature_ratio,
                             const uint8_t window_size   ,
                             const uint8_t peak_number   ):
-    AudioBase(downsmp_freq, fir_coefs, frame_duration, feature_ratio, window_size, peak_number),
-    audio_db_matcher_(AudioDBMatcher(db_path))
+    AudioBase(  downsmp_freq    ,
+                fir_coefs       ,
+                frame_duration  ,
+                feature_ratio   ,
+                window_size     ,
+                peak_number     ),
+    audio_db_matcher_(AudioDBMatcher(   db_path         ,
+                                        downsmp_freq    ,
+                                        fir_coefs       ,
+                                        frame_duration  ,
+                                        feature_ratio   ,
+                                        window_size     ,
+                                        peak_number     ))
 {}
 
 std::string AudioMatcher::match(const std::string& file_path)
