2fac675e16ced6ccd510e8a9e8e6aebe618cab00
Jon <jon_portugalete@hotmail.com>
2026-07-06 18:43:56 +0200

    Used Impl technique so as to hide all of the undisclosed header files to the public file.

--- src/sonora.cpp
+++ src/sonora.cpp
@@ -1,17 +1,74 @@
+#include "audio_indexer.hpp"
+#include "audio_matcher.hpp"
+#include "scheduler.hpp"
 #include "sonora.hpp"
 
-Sonora::Sonora( const uint32_t downsmp_freq                 ,
-                const std::string& db_path                  ,
-                const std::size_t fir_coefs                 ,
-                const float frame_duration                  ,
-                const uint32_t feature_ratio                ,
-                const uint8_t window_size                   ,
-                const uint8_t peak_number                   ,
-                const uint64_t max_index_rqs                ,
-                const std::chrono::minutes index_expire_mins,
-                const uint64_t max_match_rqs                ,
-                const std::chrono::minutes match_expire_mins,
-                const uint64_t max_match_threads            ):
+class Sonora::Impl
+{
+public:
+    using op_time_t = std::chrono::steady_clock::time_point;
+
+    // Indexing-side definitions
+    typedef struct
+    {
+        request_status_t status;
+        op_time_t expire_time;
+    } index_op_info_t;
+
+    // Indexing-side variables
+    AudioIndexer audio_indexer_;
+
+    // Matching-side definitions
+    typedef struct
+    {
+        request_status_t status;
+        op_time_t expire_time;
+        std::string ret;
+    } match_op_info_t;
+
+    // Matching-side variables
+    AudioMatcher audio_matcher_;
+
+    // Indexing-side functions
+    void _indexRoutine(void);
+
+    // Matching-side functions
+    void _matchRoutine(void);
+
+    std::function<std::optional<std::string>(const std::string&)> index_worker_;
+    std::function<void(index_op_info_t&, const std::optional<std::string>&)> index_saver_;
+    Scheduler<index_op_info_t> index_scheduler_;
+    
+    std::function<std::optional<std::string>(const std::string&)> match_worker_;
+    std::function<void(match_op_info_t&, const std::optional<std::string>&)> match_saver_;
+    Scheduler<match_op_info_t> match_scheduler_;
+
+    Impl(const uint32_t downsmp_freq                 ,
+        const std::string& db_path                  ,
+        const std::size_t fir_coefs                 ,
+        const float frame_duration                  ,
+        const uint32_t feature_ratio                ,
+        const uint8_t window_size                   ,
+        const uint8_t peak_number                   ,
+        const uint64_t max_index_rqs                ,
+        const std::chrono::minutes index_expire_mins,
+        const uint64_t max_match_rqs                ,
+        const std::chrono::minutes match_expire_mins,
+        const uint64_t max_match_threads            );
+};
+
+Sonora::Impl::Impl( const uint32_t downsmp_freq                 ,
+                    const std::string& db_path                  ,
+                    const std::size_t fir_coefs                 ,
+                    const float frame_duration                  ,
+                    const uint32_t feature_ratio                ,
+                    const uint8_t window_size                   ,
+                    const uint8_t peak_number                   ,
+                    const uint64_t max_index_rqs                ,
+                    const std::chrono::minutes index_expire_mins,
+                    const uint64_t max_match_rqs                ,
+                    const std::chrono::minutes match_expire_mins,
+                    const uint64_t max_match_threads            ):
     audio_indexer_(AudioIndexer(downsmp_freq    ,
                                 db_path         ,
                                 fir_coefs       ,
@@ -54,6 +111,32 @@ Sonora::Sonora( const uint32_t downsmp_freq                 ,
                         max_match_threads       )
 {}
 
+Sonora::Sonora( const uint32_t downsmp_freq                 ,
+                const std::string& db_path                  ,
+                const std::size_t fir_coefs                 ,
+                const float frame_duration                  ,
+                const uint32_t feature_ratio                ,
+                const uint8_t window_size                   ,
+                const uint8_t peak_number                   ,
+                const uint64_t max_index_rqs                ,
+                const std::chrono::minutes index_expire_mins,
+                const uint64_t max_match_rqs                ,
+                const std::chrono::minutes match_expire_mins,
+                const uint64_t max_match_threads            ):
+    impl_(std::make_unique<Sonora::Impl>(   downsmp_freq        ,
+                                            db_path             ,
+                                            fir_coefs           ,
+                                            frame_duration      ,
+                                            feature_ratio       ,
+                                            window_size         ,
+                                            peak_number         ,
+                                            max_index_rqs       ,
+                                            index_expire_mins   ,
+                                            max_match_rqs       ,
+                                            match_expire_mins   ,
+                                            max_match_threads   ))
+{}
+
 Sonora::~Sonora(void)
 {
     end();
@@ -61,57 +144,57 @@ Sonora::~Sonora(void)
 
 void Sonora::run(void)
 {
-    index_scheduler_.run();
-    match_scheduler_.run();
+    impl_->index_scheduler_.run();
+    impl_->match_scheduler_.run();
 }
 
 void Sonora::end(void)
 {
-    index_scheduler_.end();
-    match_scheduler_.end();
+    impl_->index_scheduler_.end();
+    impl_->match_scheduler_.end();
 }
 
 std::optional<uint64_t> Sonora::index(const std::string& file_path)
 {
-    return index_scheduler_.enqueueJob(file_path);
+    return impl_->index_scheduler_.enqueueJob(file_path);
 }
 
 bool Sonora::hasPendingIndexOps(void) const
 {
-    return index_scheduler_.hasPendingOps();
+    return impl_->index_scheduler_.hasPendingOps();
 }
 
 bool Sonora::hasOngoingIndexOps(void) const
 {
-    return index_scheduler_.hasOngoingOps();
+    return impl_->index_scheduler_.hasOngoingOps();
 }
 
 request_status_t Sonora::getIndexStatus(const uint64_t job_id)
 {
-    return index_scheduler_.getJobStatus(job_id);
+    return impl_->index_scheduler_.getJobStatus(job_id);
 }
 
 std::optional<uint64_t> Sonora::match(const std::string& file_path)
 {
-    return match_scheduler_.enqueueJob(file_path);
+    return impl_->match_scheduler_.enqueueJob(file_path);
 }
 
 bool Sonora::hasPendingMatchOps(void) const
 {
-    return match_scheduler_.hasPendingOps();
+    return impl_->match_scheduler_.hasPendingOps();
 }
 
 bool Sonora::hasOngoingMatchOps(void) const
 {
-    return match_scheduler_.hasOngoingOps();
+    return impl_->match_scheduler_.hasOngoingOps();
 }
 
 request_status_t Sonora::getMatchStatus(const uint64_t job_id)
 {
-    return match_scheduler_.getJobStatus(job_id);
+    return impl_->match_scheduler_.getJobStatus(job_id);
 }
 
 std::string Sonora::getMatchResult(const uint64_t job_id) const
 {
-    return match_scheduler_.getJobResult(job_id).ret;
+    return impl_->match_scheduler_.getJobResult(job_id).ret;
 }
