0bffb8dfc7ff8a0b56fdef0b3952736fc9d25595
Jon <jon_portugalete@hotmail.com>
2026-07-09 12:28:42 +0200

    Added cosine signal test section as well as new section for basic FFT properties (only the very basics will be tested since the function returns power spectrum computation).

--- tests/unit/src/test_fft_processor.cpp
+++ tests/unit/src/test_fft_processor.cpp
@@ -1,6 +1,9 @@
 #include <catch2/catch.hpp>
 #include <cstddef>
 #include <cstdint>
+#include <cmath>
+#include <algorithm>
+#include <numbers>
 #include "fft_processor.hpp"
 
 TEST_CASE("FFT Processor: Constructor with custom/default parameters", "[FFT Processor][Constructor]")
@@ -34,10 +37,43 @@ TEST_CASE("FFT Processor: Constructor with custom/default parameters", "[FFT Pro
     }
 }
 
-static void requireOnlyPositiveOrNullBins(const std::vector<float>& bins)
+static inline void requireOnlyPositiveOrNullBins(const std::vector<float>& vec)
 {
-    for(std::size_t idx = 0; idx < bins.size(); idx++)
-        REQUIRE(bins[idx] >= .0f);
+    REQUIRE(std::all_of(vec.begin(), vec.end(), [](const float& f) -> bool { return f >= 0; }));
+}
+
+// Implements cos(2πK/N)
+static std::vector<float> makeCosine(const std::size_t N, const std::size_t K)
+{
+    std::vector<float> ret(N);
+
+    for (std::size_t n = 0; n < N; ++n)
+        ret[n] = std::cos(2.0f * std::numbers::pi_v<float> * K * n / static_cast<float>(N));
+
+    return ret;
+}
+
+static void allElementsZeroButOne(const std::vector<float>& vec)
+{
+    const float threshold = 10e-9;
+    
+    REQUIRE(std::count_if(vec.begin(), vec.end(), [threshold](const float& f) -> bool { return f > threshold; }) == 1);
+}
+
+// Check whether scaled_vec == (normal_vec * scale)
+static void allElementsAreScaled(std::vector<float> normal_vec, std::vector<float> scaled_vec, const float scale)
+{
+    REQUIRE(normal_vec.size() == scaled_vec.size());
+
+    auto sqrt_elem = [](const float& f) -> float { return std::sqrt(f); };
+    
+    std::transform(normal_vec.begin(), normal_vec.end(), normal_vec.begin(), sqrt_elem);
+    std::transform(scaled_vec.begin(), scaled_vec.end(), scaled_vec.begin(), sqrt_elem);
+
+    for(float& f : normal_vec)
+        f *= scale;
+
+    REQUIRE(normal_vec == scaled_vec);
 }
 
 TEST_CASE("FFT Processor: FFT", "[FFT Processor][FFT]")
@@ -49,26 +85,26 @@ TEST_CASE("FFT Processor: FFT", "[FFT Processor][FFT]")
     FFTProcessor fft_proc(frame_duration, sampling_frequency, feature_ratio); // Samples per frame: 200
     std::vector<float> frame;
     const uint32_t fft_samples_per_frame = frame_duration * sampling_frequency;
-    const std::size_t samples_per_frame = static_cast<std::size_t>(fft_samples_per_frame);
+    const uint32_t number_of_bins = ((frame_duration * sampling_frequency) / 2) + 1;
 
     SECTION("Test frame size")
     {
         SECTION("Samples in frame == expected")
         {
             frame.resize(static_cast<std::size_t>(fft_samples_per_frame));
-            REQUIRE_NOTHROW(fft_proc.FFT(frame));
+            REQUIRE_NOTHROW(fft_proc.computePowerSpectrum(frame));
         }
 
         SECTION("Samples in frame > expected")
         {
             frame.resize(static_cast<std::size_t>(fft_samples_per_frame + 1));
-            REQUIRE_THROWS_AS(fft_proc.FFT(frame), std::invalid_argument);
+            REQUIRE_THROWS_AS(fft_proc.computePowerSpectrum(frame), std::invalid_argument);
         }
 
         SECTION("Samples in frame < expected")
         {
             frame.resize(static_cast<std::size_t>(fft_samples_per_frame - 1));
-            REQUIRE_THROWS_AS(fft_proc.FFT(frame), std::invalid_argument);
+            REQUIRE_THROWS_AS(fft_proc.computePowerSpectrum(frame), std::invalid_argument);
         }
     }
 
@@ -77,29 +113,63 @@ TEST_CASE("FFT Processor: FFT", "[FFT Processor][FFT]")
         SECTION("All zeros")
         {
             frame = std::vector<float>(fft_samples_per_frame, .0f);
-            std::vector<float> fft_ret = fft_proc.FFT(frame);
+            std::vector<float> fft_ret = fft_proc.computePowerSpectrum(frame);
             requireOnlyPositiveOrNullBins(fft_ret);
-            REQUIRE(frame == fft_proc.FFT(frame));
+            REQUIRE(fft_ret == std::vector<float>(number_of_bins, .0f));
         }
 
         SECTION("Unit impulse")
         {
             frame = std::vector<float>(fft_samples_per_frame, .0f);
             frame[0] = 1;
-            std::vector<float> fft_ret = fft_proc.FFT(frame);
+            std::vector<float> fft_ret = fft_proc.computePowerSpectrum(frame);
             requireOnlyPositiveOrNullBins(fft_ret);
-            REQUIRE(fft_ret == std::vector<float>(fft_samples_per_frame, 1.0f));
+            REQUIRE(fft_ret == std::vector<float>(number_of_bins, 1.0f));
         }
 
         SECTION("Constant signal")
         {
             frame = std::vector<float>(fft_samples_per_frame, 2.0f);
-            std::vector<float> fft_ret = fft_proc.FFT(frame);
+            std::vector<float> fft_ret = fft_proc.computePowerSpectrum(frame);
             requireOnlyPositiveOrNullBins(fft_ret);
-            REQUIRE(fft_ret[0] == 4.0f);
+            REQUIRE(fft_ret[0] != 4.0f);
+            REQUIRE(std::all_of(fft_ret.begin() + 1, fft_ret.end(), [](const float& f) -> bool { return (f >= Approx(.0f)); }));
+        }
 
-            for(std::size_t idx = 1; idx < fft_ret.size(); idx++)
-                REQUIRE(fft_ret[idx] == Approx(.0f));
+        SECTION("Cosine signal")
+        {
+            frame = makeCosine(fft_samples_per_frame, 5);
+            std::vector<float> fft_ret = fft_proc.computePowerSpectrum(frame);
+            requireOnlyPositiveOrNullBins(fft_ret);
+            allElementsZeroButOne(fft_ret);
+        }
+    }
+
+    SECTION("Basic properties")
+    {
+        SECTION("Amplitude scaling")
+        {
+            const float amp = 2.0f;
+
+            frame = std::vector<float>(fft_samples_per_frame, amp);
+            std::vector<float> fft_ret_0 = fft_proc.computePowerSpectrum(frame);
+
+            const float scale = 3.0f;
+
+            frame = std::vector<float>(fft_samples_per_frame, scale * amp);
+            std::vector<float> fft_ret_1 = fft_proc.computePowerSpectrum(frame);
+
+            allElementsAreScaled(fft_ret_0, fft_ret_1, scale);
+        }
+
+        SECTION("Linearity")
+        {
+
+        }
+
+        SECTION("Circular shift")
+        {
+
         }
     }
 }
