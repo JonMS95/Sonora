97dc9fe9a88951160179daf92bfeb68522ba0d62
Jon <jon_portugalete@hotmail.com>
2026-07-13 18:11:19 +0200

    Added isIndexerRunning and isMatcherRunning methods.

--- include/sonora.hpp
+++ include/sonora.hpp
@@ -38,11 +38,13 @@ public:
     std::optional<uint64_t> index(const std::string& file_path);
     bool hasPendingIndexOps(void) const;
     bool hasOngoingIndexOps(void) const;
+    bool isIndexerRunning(void) const;
     request_status_t getIndexStatus(const uint64_t job_id);
 
     std::optional<uint64_t> match(const std::string& file_path);
     bool hasPendingMatchOps(void) const;
     bool hasOngoingMatchOps(void) const;
+    bool isMatcherRunning(void) const;
     request_status_t getMatchStatus(const uint64_t job_id);
     std::string getMatchResult(const uint64_t job_id) const;
 };
