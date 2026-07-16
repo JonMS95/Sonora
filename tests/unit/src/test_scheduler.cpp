157042ac831061cfc28843adc209469a8453c43e
Jon <jon_portugalete@hotmail.com>
2026-07-16 18:56:46 +0200

    Finished Scheduler template class tests.

--- tests/unit/src/test_scheduler.cpp
+++ tests/unit/src/test_scheduler.cpp
@@ -7,23 +7,12 @@
 #include <functional>
 #include <chrono>
 #include <optional>
-// #include "test_db_helper.hpp"
-#include "audio_indexer.hpp"
-#include "audio_matcher.hpp"
 #include "rq_status_enum.hpp"
 #include "scheduler.hpp"
 
 static const std::filesystem::path test_data_dir_path = std::filesystem::path(TEST_DATA_DIR);
-static const std::filesystem::path db_dir_path = test_data_dir_path / "db";
 static const std::filesystem::path full_samples_dir_path = test_data_dir_path / "samples" / "full_samples";
 static const std::filesystem::path samples_path = full_samples_dir_path / "sample_3s_16_khz.wav";
-
-static const std::string& samples_db_path = std::string(db_dir_path / "sample_fingerprints.db");
-
-static const std::string& dummy_db_base = "dummy_test_audio_indexer.db";
-static const std::string& dummy_db_path = std::string(db_dir_path / dummy_db_base);
-static const std::string& dummy_db_wal_path = std::string(db_dir_path / (dummy_db_base + "-wal"));
-static const std::string& dummy_db_shm_path = std::string(db_dir_path / (dummy_db_base + "-shm"));
 
 const uint64_t max_rqs                  = 10;
 const std::chrono::minutes expire_mins  = std::chrono::minutes{10};
@@ -39,7 +28,7 @@ typedef struct
 static std::function<Scheduler<op_info_t>::work_fn_sig_t> fn_worker =
     [](const std::string& file) -> std::optional<std::string>
     {
-        return ("dummy" + file);
+        return ("dummy_" + file);
     };
 
 static std::function<Scheduler<op_info_t>::save_fn_sig_t> fn_saver = 
@@ -47,6 +36,13 @@ static std::function<Scheduler<op_info_t>::save_fn_sig_t> fn_saver =
     {
         if(ret.has_value())
             op.ret = ret.value();
+    };
+
+static std::function<Scheduler<op_info_t>::work_fn_sig_t> fn_sleep =
+    [](const std::string& file) -> std::optional<std::string>
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        return ("dummy" + file);
     };
 
 TEST_CASE("Scheduler: Constructor with custom parameters", "[Scheduler][Constructor]")
@@ -225,13 +221,6 @@ TEST_CASE("Scheduler: getJobStatus", "[Scheduler][getJobStatus]")
         REQUIRE(scheduler.getJobStatus(0) == request_status_t::OP_UNKNOWN);
     }
 
-    static std::function<Scheduler<op_info_t>::work_fn_sig_t> fn_sleep =
-    [](const std::string& file) -> std::optional<std::string>
-    {
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-        return ("dummy" + file);
-    };
-
     SECTION("Spot ongoing job")
     {
         Scheduler<op_info_t> scheduler( fn_sleep    ,
@@ -266,5 +255,150 @@ TEST_CASE("Scheduler: getJobStatus", "[Scheduler][getJobStatus]")
         uint64_t job_id = scheduler.enqueueJob(std::string(samples_path)).value();
 
         REQUIRE(scheduler.getJobStatus(job_id) == request_status_t::OP_QUEUED);
+    }
+}
+
+TEST_CASE("Scheduler: hasPendingOps", "[Scheduler][hasPendingOps]")
+{
+    SECTION("Has pending jobs")
+    {
+        Scheduler<op_info_t> scheduler( fn_sleep    ,
+                                        fn_saver    ,
+                                        max_rqs     ,
+                                        expire_mins ,
+                                        max_threads );
+        scheduler.run();
+
+        uint64_t job_id = scheduler.enqueueJob(std::string(samples_path)).value();
+
+        while(scheduler.getJobStatus(job_id) == request_status_t::OP_UNKNOWN)
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+        REQUIRE(scheduler.hasPendingOps());
+    }
+
+    SECTION("Has no pending jobs")
+    {
+        Scheduler<op_info_t> scheduler( fn_worker   ,
+                                        fn_saver    ,
+                                        max_rqs     ,
+                                        expire_mins ,
+                                        max_threads );
+        scheduler.run();
+
+        uint64_t job_id = scheduler.enqueueJob(std::string(samples_path)).value();
+
+        while(  (scheduler.getJobStatus(job_id) == request_status_t::OP_UNKNOWN ) ||
+                (scheduler.getJobStatus(job_id) == request_status_t::OP_QUEUED  ))
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+        REQUIRE_FALSE(scheduler.hasPendingOps());
+    }
+}
+
+TEST_CASE("Scheduler: hasOngoingOps", "[Scheduler][hasOngoingOps]")
+{
+    SECTION("Has ongoing jobs")
+    {
+        Scheduler<op_info_t> scheduler( fn_sleep    ,
+                                        fn_saver    ,
+                                        max_rqs     ,
+                                        expire_mins ,
+                                        max_threads );
+        scheduler.run();
+
+        uint64_t job_id = scheduler.enqueueJob(std::string(samples_path)).value();
+
+        while((scheduler.getJobStatus(job_id) == request_status_t::OP_UNKNOWN) || (scheduler.getJobStatus(job_id) == request_status_t::OP_QUEUED))
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+        REQUIRE(scheduler.hasOngoingOps());
+    }
+
+    SECTION("Has no ongoing jobs")
+    {
+        Scheduler<op_info_t> scheduler( fn_worker   ,
+                                        fn_saver    ,
+                                        max_rqs     ,
+                                        expire_mins ,
+                                        max_threads );
+        scheduler.run();
+
+        uint64_t job_id = scheduler.enqueueJob(std::string(samples_path)).value();
+
+        while(  (scheduler.getJobStatus(job_id) == request_status_t::OP_UNKNOWN ) ||
+                (scheduler.getJobStatus(job_id) == request_status_t::OP_QUEUED  ) ||
+                (scheduler.getJobStatus(job_id) == request_status_t::OP_ONGOING ))
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+        REQUIRE_FALSE(scheduler.hasOngoingOps());
+    }
+}
+
+TEST_CASE("Scheduler: isSchedulerRunning", "[Scheduler][isSchedulerRunning]")
+{
+    Scheduler<op_info_t> scheduler( fn_worker   ,
+                                    fn_saver    ,
+                                    max_rqs     ,
+                                    expire_mins ,
+                                    max_threads );
+    
+    SECTION("Is running after run is called")
+    {
+        scheduler.run();
+
+        REQUIRE(scheduler.isSchedulerRunning());
+    }
+
+    SECTION("Is not running if run is not called")
+    {
+        REQUIRE_FALSE(scheduler.isSchedulerRunning());
+    }
+
+    SECTION("Is not running after end is called")
+    {
+        scheduler.run();
+        scheduler.end();
+
+        REQUIRE_FALSE(scheduler.isSchedulerRunning());
+    }
+}
+
+TEST_CASE("Scheduler: getJobResult", "[Scheduler][getJobResult]")
+{
+    SECTION("Do the whole job then get the result")
+    {
+        Scheduler<op_info_t> scheduler( fn_worker   ,
+                                        fn_saver    ,
+                                        max_rqs     ,
+                                        expire_mins ,
+                                        max_threads );
+
+        const std::string& path_str = std::string(samples_path);
+
+        scheduler.run();
+        uint64_t job_id = scheduler.enqueueJob(path_str).value();
+
+        while(scheduler.getJobStatus(job_id) != request_status_t::OP_OK)
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        
+        REQUIRE(scheduler.getJobResult(job_id).ret == ("dummy_" + path_str));
+    }
+
+    SECTION("Do the job but ask for the result early")
+    {
+        Scheduler<op_info_t> scheduler( fn_sleep    ,
+                                        fn_saver    ,
+                                        max_rqs     ,
+                                        expire_mins ,
+                                        max_threads );
+
+        const std::string& path_str = std::string(samples_path);
+
+        scheduler.run();
+        uint64_t job_id = scheduler.enqueueJob(path_str).value();
+        
+        REQUIRE_FALSE(scheduler.getJobStatus(job_id) == request_status_t::OP_OK);
+        REQUIRE(scheduler.getJobResult(job_id).ret == "");
     }
 }
